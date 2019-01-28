eb32ec38add9bd631dcd86f6f60dbef0f60bb5a4
Ronald Maj <36747983+ronaldmaj@users.noreply.github.com>
2019-01-28 14:53:26 +1100

    Further work on group_by function

--- python-exercises/group_by.py
+++ python-exercises/group_by.py
@@ -2,6 +2,10 @@
 
 def group_by(iterable, func):
 	final = dict()
+	def def_func(n):
+		return n 
+	if func == None:
+		func = def_func
 	for item in iterable:
 		if func(item) not in final: 
 			final[func(item)] = []
