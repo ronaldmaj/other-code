2919b5c407f6303be307e531ff02cf83f2c0da68
Ronald Maj <36747983+ronaldmaj@users.noreply.github.com>
2019-01-29 09:56:31 +1100

    Worked on group_by

--- python-exercises/group_by.py
+++ python-exercises/group_by.py
@@ -1,13 +1,17 @@
 # Attempt at Python Morsel exercise - group_by
 
-def group_by(iterable, func):
+def group_by(iterable, func=None):
 	final = dict()
 	def def_func(n):
 		return n 
 	if func == None:
-		func = def_func
-	for item in iterable:
-		if func(item) not in final: 
-			final[func(item)] = []
-		final[func(item)].append(item)
+		for item in iterable:
+			if def_func(item) not in final: 
+				final[def_func(item)] = []
+			final[def_func(item)].append(item)
+	else:
+		for item in iterable:
+			if func(item) not in final: 
+				final[func(item)] = []
+			final[func(item)].append(item)
 	return final	
