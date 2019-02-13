ef50b7d921b81b2df1c79ed150af3ea284f59a3c
Ronald Maj <36747983+ronaldmaj@users.noreply.github.com>
2019-02-13 22:40:26 +1100

    Started python exercise - Point

---
+++ python-exercises/Point.py
@@ -0,0 +1,31 @@
+# Attempt at Python Morsels exercise 'Point'
+# create Point class:
+
+# Attempt at base exercise - passed tests
+class Point:
+	
+	def __init__(self, x, y, z):
+		self.x = x 
+		self.y = y
+		self.z = z
+	
+	def __repr__(self):
+		return f'Point({self.x}, {self.y}, {self.z})'
+		
+	def __eq__(self, other):
+		return self.x == other.x and self.y == other.y and self.z == other.z
+		
+	def __ne__(self, other):
+		return self.x != other.x or self.y != other.y or self.z != other.z	
+	
+	# Attempt at bonus 1 - passed tests:
+	def __add__(self, other):
+		return Point(self.x+other.x, self.y+other.y, self.z+other.z)
+		
+	def __sub__(self, other):
+		return Point(self.x-other.x, self.y-other.y, self.z-other.z)
+	# Attempt at bonus 2 -
+	def __mul__(self, other):
+		return Point(self.x * other, self.y * other, self.z * other)
+ 
+
