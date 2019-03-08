80239f7529bd69650492964fc538c9c8af695d2d
Ronald Maj <36747983+ronaldmaj@users.noreply.github.com>
2019-03-08 19:47:09 +1100

    Editing argparse and name check

--- python-exercises/unicode_escape.py
+++ python-exercises/unicode_escape.py
@@ -1,23 +1,24 @@
 # Attempt at unicode_escape Python Morsel after looking at solutions
 
 
-import argparse
+if __name__ == '__main__':
 
+	import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input_file")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_file", type=argparse.FileType('rt'))
 
-args = parser.parse_args()
+	args = parser.parse_args()
 
-in_file = args.input_file
+	in_file = args.input_file
 
-with open(in_file, mode='rt', encoding='utf-8') as f:
-	data = f.read()
-	escape_str = []
-	for c in data:
-		if ord(c) > 127:
-			code = hex(ord(c))[2:]
-			escape_str.append(rf"\U{code:0>8}")
-		else:
-			escape_str.append(c)
-	print(''.join(escape_str), end='')
+	with open(in_file, mode='rt', encoding='utf-8') as f:
+		data = f.read()
+		escape_str = []
+		for c in data:
+			if ord(c) > 127:
+				code = hex(ord(c))[2:]
+				escape_str.append(rf"\U{code:0>8}")
+			else:
+				escape_str.append(c)
+		print(''.join(escape_str), end='')
